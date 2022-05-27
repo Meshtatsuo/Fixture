2550c3d992084f426f3ec95234c467cc73a13c85
meshtatsuo <meshtatsuo@gmail.com>
2022-05-26 19:48:09 -0600

    react and tailwind up and running!

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,4 @@
-const {
-  AuthenticationError
-} = require('apollo-server-express');
+const { AuthenticationError } = require("apollo-server-express");
 // const {
 //   User,
 //   Product
@@ -14,10 +12,7 @@ const {
 
 const resolvers = {
   Query: {
-    products: async (parent, {
-      category,
-      name
-    }) => {
+    products: async (parent, { category, name }) => {
       const params = {};
 
       if (category) {
@@ -26,21 +21,19 @@ const resolvers = {
 
       if (name) {
         params.name = {
-          $regex: name
+          $regex: name,
         };
       }
 
-      return await Product.find(params).populate('category');
+      return await Product.find(params).populate("category");
     },
-    product: async (parent, {
-      _id
-    }) => {
-      return await Product.findById(_id).populate('category');
+    product: async (parent, { _id }) => {
+      return await Product.findById(_id).populate("category");
     },
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'orders.products'
+          path: "orders.products",
         });
 
         user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
@@ -48,29 +41,25 @@ const resolvers = {
         return user;
       }
 
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError("Not logged in");
     },
-    order: async (parent, {
-      _id
-    }, context) => {
+    order: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'orders.products'
+          path: "orders.products",
         });
 
         return user.orders.id(_id);
       }
 
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError("Not logged in");
     },
 
     checkout: async (parent, args, context) => {
       const order = new Order({
-        products: args.products
+        products: args.products,
       });
-      const {
-        products
-      } = await order.populate('products');
+      const { products } = await order.populate("products");
 
       const line_items = [];
       const url = new URL(context.headers.referer).origin;
@@ -80,38 +69,35 @@ const resolvers = {
         const product = await stripe.products.create({
           name: products[i].name,
           description: products[i].description,
-          images: [`${url}/images/${products[i].image}`]
+          images: [`${url}/images/${products[i].image}`],
         });
 
         // generate price id using the product id
         const price = await stripe.prices.create({
           product: product.id,
           unit_amount: products[i].price * 100,
-          currency: 'usd',
+          currency: "usd",
         });
 
         // add price id to the line items array
         line_items.push({
           price: price.id,
-          quantity: 1
+          quantity: 1,
         });
       }
 
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
+        payment_method_types: ["card"],
         line_items,
-        mode: 'payment',
+        mode: "payment",
         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`
+        cancel_url: `${url}/`,
       });
 
       return {
-        session: session.id
+        session: session.id,
       };
-
-
-    }
-
+    },
   },
   Mutation: {
     addUser: async (parent, args) => {
@@ -120,78 +106,74 @@ const resolvers = {
 
       return {
         token,
-        user
+        user,
       };
     },
-    addOrder: async (parent, {
-      products
-    }, context) => {
+    addOrder: async (parent, { products }, context) => {
       console.log(context);
       if (context.user) {
         const order = new Order({
-          products
+          products,
         });
 
         await User.findByIdAndUpdate(context.user._id, {
           $push: {
-            orders: order
-          }
+            orders: order,
+          },
         });
 
         return order;
       }
 
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError("Not logged in");
     },
     updateUser: async (parent, args, context) => {
       if (context.user) {
         return await User.findByIdAndUpdate(context.user._id, args, {
-          new: true
+          new: true,
         });
       }
 
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError("Not logged in");
     },
-    updateProduct: async (parent, {
-      _id,
-      quantity
-    }) => {
+    updateProduct: async (parent, { _id, quantity }) => {
       const decrement = Math.abs(quantity) * -1;
 
-      return await Product.findByIdAndUpdate(_id, {
-        $inc: {
-          quantity: decrement
+      return await Product.findByIdAndUpdate(
+        _id,
+        {
+          $inc: {
+            quantity: decrement,
+          },
+        },
+        {
+          new: true,
         }
-      }, {
-        new: true
-      });
+      );
     },
-    login: async (parent, {
-      email,
-      password
-    }) => {
+    login: async (parent, { email, password }) => {
       const user = await User.findOne({
-        email
+        email,
       });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
 
       return {
         token,
-        user
+        user,
       };
-    }
-  }
+    },
+  },
 };
 
 module.exports = resolvers;
