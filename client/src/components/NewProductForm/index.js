f3536e059938983b610def7b0e2c483d6da5381d
meshtatsuo <meshtatsuo@gmail.com>
2022-05-31 12:06:47 -0600

    file downloading

--- client/src/components/NewProductForm/index.js
+++ client/src/components/NewProductForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import axios from "axios";
+import { saveAs } from "file-saver";
 
 import { ADD_PRODUCT } from "../../utils/mutations";
 
@@ -150,120 +151,140 @@ const NewProductForm = () => {
     }
   };
 
+  const downloadProduct = async (event) => {
+    console.log("time to download");
+    let key = "330e9c3ca30602cc4d465f7afe07acc9";
+    let fileName = "testFile.zip";
+
+    const url = await axios.get(`download/${key}/${fileName}`, {
+      responseType: "stream",
+    });
+
+    if (!url) {
+      console.log("An error has occurred");
+    } else {
+      console.log(url);
+      saveAs(url.data);
+    }
+  };
+
   return (
-    <form onSubmit={handleFormSubmit}>
-      <div id="productTitle" className="flex">
-        <label
-          htmlFor="productTitle"
-          className="font-bold align-middle text-2xl p-5 "
-        >
-          Product Title:
-        </label>
-        <input
-          placeholder="My Product"
-          name="productTitle"
-          type="text"
-          onChange={handleChange}
-          className="my-4 mr-5 p-3 flex-1 align-middle bg-orange-100 rounded border-solid"
-        />
-      </div>
-
-      <div id="productDescription" className="flex flex-wrap">
-        <label
-          htmlFor="productDescription"
-          className="font-bold align-middle text-2xl p-5 w-full"
-        >
-          Product Description:
-        </label>
-        <textarea
-          placeholder="My Product Description"
-          name="productDescription"
-          type="text"
-          onChange={handleChange}
-          className="mx-5 p-3 w-full bg-orange-100 rounded"
-          rows="8"
-        />
-      </div>
-      <div id="productPrice" className="flex">
-        <label
-          htmlFor="productPrice"
-          className="font-bold align-middle text-2xl p-5 flex-0"
-        >
-          Product Price:
-        </label>
-        <label className="font-bold text-2xl p-5 flex-0">$</label>
-
-        <input
-          placeholder="10.00"
-          name="productPrice"
-          id="product-price"
-          type="text"
-          onChange={handleChange}
-          className="my-3 p-3 mr-5 flex-1 align-middle bg-orange-100 rounded border-solid "
-        />
-      </div>
-      <h2 className="font-bold align-middle text-4xl px-10 pt-10 pb-2">
-        Upload Files:
-      </h2>
-      <p className="font-bold align-middle text-s px-10 py-3 flex-0">
-        *If your product contains multiple files, please add them to a .zip file
-        before uploading!
-      </p>
-      <div id="productFile">
-        <label
-          htmlFor="productFile"
-          className="font-bold align-middle text-2xl p-5"
-        >
-          Product File:
-        </label>
-        <p className="font-bold align-middle text-s px-10 py-2 flex-0">
-          *Accepts: .png, .jpg, .jpeg, .zip, .pdf
-        </p>
-        <input
-          type="file"
-          name="productFile"
-          id="product-upload"
-          accept=".png, .jpg, .jpeg, .png, .zip, .pdf "
-          onChange={handleChange}
-          className="font-bold align-middle text-md p-5"
-        />
-      </div>
-      <div id="productThumbnail">
-        <label
-          htmlFor="productThumbnail"
-          className="font-bold align-middle text-2xl p-5"
-        >
-          Product Thumbnail:
-        </label>
-        <p className="font-bold align-middle text-s px-10 py-4 flex-0">
-          *Use 500x500px image for best results!
-        </p>
-        <p className="font-bold align-middle text-s px-10 pb-4 flex-0">
-          *Accepts: .png, .jpg, .jpeg
-        </p>
-        <input
-          type="file"
-          name="productThumbnail"
-          id="thumbnail-upload"
-          accept=".png, .jpg, .jpeg"
-          onChange={handleChange}
-          className="font-bold align-middle text-md p-5"
-        />
-      </div>
-      {errorMessage && (
-        <div>
-          <p className="error-text font-bold align-middle text-s px-10 pb-5 text-red-700">
-            {errorMessage}
-          </p>
+    <>
+      <button onClick={downloadProduct}>Download Product</button>
+      <form onSubmit={handleFormSubmit}>
+        <div id="productTitle" className="flex">
+          <label
+            htmlFor="productTitle"
+            className="font-bold align-middle text-2xl p-5 "
+          >
+            Product Title:
+          </label>
+          <input
+            placeholder="My Product"
+            name="productTitle"
+            type="text"
+            onChange={handleChange}
+            className="my-4 mr-5 p-3 flex-1 align-middle bg-orange-100 rounded border-solid"
+          />
         </div>
-      )}
-      <button
-        className="font-bold align-middle text-s px-10 pb-5 bg-orange-100 rounded border-black"
-        onClick={handleFormSubmit}
-      >
-        <p className="font-bold"> Create Product </p>
-      </button>
-    </form>
+
+        <div id="productDescription" className="flex flex-wrap">
+          <label
+            htmlFor="productDescription"
+            className="font-bold align-middle text-2xl p-5 w-full"
+          >
+            Product Description:
+          </label>
+          <textarea
+            placeholder="My Product Description"
+            name="productDescription"
+            type="text"
+            onChange={handleChange}
+            className="mx-5 p-3 w-full bg-orange-100 rounded"
+            rows="8"
+          />
+        </div>
+        <div id="productPrice" className="flex">
+          <label
+            htmlFor="productPrice"
+            className="font-bold align-middle text-2xl p-5 flex-0"
+          >
+            Product Price:
+          </label>
+          <label className="font-bold text-2xl p-5 flex-0">$</label>
+
+          <input
+            placeholder="10.00"
+            name="productPrice"
+            id="product-price"
+            type="text"
+            onChange={handleChange}
+            className="my-3 p-3 mr-5 flex-1 align-middle bg-orange-100 rounded border-solid "
+          />
+        </div>
+        <h2 className="font-bold align-middle text-4xl px-10 pt-10 pb-2">
+          Upload Files:
+        </h2>
+        <p className="font-bold align-middle text-s px-10 py-3 flex-0">
+          *If your product contains multiple files, please add them to a .zip
+          file before uploading!
+        </p>
+        <div id="productFile">
+          <label
+            htmlFor="productFile"
+            className="font-bold align-middle text-2xl p-5"
+          >
+            Product File:
+          </label>
+          <p className="font-bold align-middle text-s px-10 py-2 flex-0">
+            *Accepts: .png, .jpg, .jpeg, .zip, .pdf
+          </p>
+          <input
+            type="file"
+            name="productFile"
+            id="product-upload"
+            accept=".png, .jpg, .jpeg, .png, .zip, .pdf "
+            onChange={handleChange}
+            className="font-bold align-middle text-md p-5"
+          />
+        </div>
+        <div id="productThumbnail">
+          <label
+            htmlFor="productThumbnail"
+            className="font-bold align-middle text-2xl p-5"
+          >
+            Product Thumbnail:
+          </label>
+          <p className="font-bold align-middle text-s px-10 py-4 flex-0">
+            *Use 500x500px image for best results!
+          </p>
+          <p className="font-bold align-middle text-s px-10 pb-4 flex-0">
+            *Accepts: .png, .jpg, .jpeg
+          </p>
+          <input
+            type="file"
+            name="productThumbnail"
+            id="thumbnail-upload"
+            accept=".png, .jpg, .jpeg"
+            onChange={handleChange}
+            className="font-bold align-middle text-md p-5"
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text font-bold align-middle text-s px-10 pb-5 text-red-700">
+              {errorMessage}
+            </p>
+          </div>
+        )}
+        <button
+          className="font-bold align-middle text-s px-10 pb-5 bg-orange-100 rounded border-black"
+          onClick={handleFormSubmit}
+        >
+          <p className="font-bold"> Create Product </p>
+        </button>
+      </form>
+    </>
   );
 };
 
