27bf716994095ad1a99a8a20956805c78c82d261
kstensing <stensingk@gmail.com>
2022-05-30 21:41:45 -0500

    added checkout functionality into FRONTEND

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,13 +1,19 @@
-import React from "react";
-
+import React, { useEffect } from 'react';
+import { QUERY_CHECKOUT } from '../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+import { idbPromise } from '../utils/helpers';
 import CartItem from "../components/CartItem";
 import Jumbo from "../assets/images/cart-jumbo.png";
-
+import { ADD_MULTIPLE_TO_CART } from '../utils/actions';
 import Auth from "../utils/auth";
 import { useStoreContext } from "../utils/GlobalState";
 
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   // calculate totals
   function calculateTotal() {
@@ -17,6 +23,39 @@ const Cart = () => {
     });
     return sum.toFixed(2);
   }
+
+  function submitCheckout() {
+    const productIds = [];
+  
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
+  }
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
+
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get');
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+    }
+
+    if (!state.cart.length) {
+      getCart();
+    }
+  }, [state.cart.length, dispatch]);
 
   return (
     <div>
@@ -35,11 +74,11 @@ const Cart = () => {
         </div>
         <div className="flex flex-wrap">
           <p className="flex-1 w-full px-6 font-bold text-xl text-right">
-            Total:
+            Total: ${calculateTotal()}
           </p>
           <p className="px-6 font-bold text-xl">$150.00</p>
         </div>
-        <button className="font-bold float-right text-md py-3 px-6 m-5 rounded-xl bg-orange-200 border-2 border-black">
+        <button onClick={submitCheckout} className="font-bold float-right text-md py-3 px-6 m-5 rounded-xl bg-orange-200 border-2 border-black">
           Checkout
         </button>
       </div>
